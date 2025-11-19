b2eb82e2a3db647e4660b67b8a3ae1c403ff7b79
mangle-fnaf <240119641@aston.ac.uk>
2025-11-19 20:34:51 +0000

    Update ChatClient.java
    fixed some small changes

--- Client/ChatClient.java
+++ Client/ChatClient.java
@@ -1,14 +1,17 @@
+import java.io.*;
+import java.net.*;
+
 public class ChatClient {
   public static void main(String[] args) throws IOException {
+    String hostName = "localhost"
+    int portNumber = xyz;
+
 
   if (args.length != 2) {
     System.err.println(
       "Usage: java ChatClient <hostname> <port number>");
     System.exit(1);
-  }
-
-  String hostname = 12345;
-  int portNumber = Integer.parseInt(args[1]);
-
+  
   try (
     
+
